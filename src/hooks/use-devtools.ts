172e08ccac2989e25ff8e1d218ff069bca178ec4
steelydylan <ess_president@me.com>
2024-04-10 08:25:11 +0900

    fix(indent)

--- src/hooks/use-devtools.ts
+++ src/hooks/use-devtools.ts
@@ -92,14 +92,14 @@ ${dispatchKeyboardEventToParentZoomState()}
 <body class="undocked" id="-blink-dev-tools">`;
 
 export function useDevtools() {
-	const [url, setUrl] = useState<string | null>(null);
-	useEffect(() => {
-		const devtoolsRawUrl = URL.createObjectURL(
-			new Blob([devToolHTML], { type: "text/html" }),
-		);
-		setUrl(devtoolsRawUrl);
-		return () => URL.revokeObjectURL(devtoolsRawUrl);
-	}, []);
+  const [url, setUrl] = useState<string | null>(null);
+  useEffect(() => {
+    const devtoolsRawUrl = URL.createObjectURL(
+      new Blob([devToolHTML], { type: "text/html" })
+    );
+    setUrl(devtoolsRawUrl);
+    return () => URL.revokeObjectURL(devtoolsRawUrl);
+  }, []);
 
-	return `${url}#?embedded=${encodeURIComponent(location.origin)}`;
+  return `${url}#?embedded=${encodeURIComponent(location.origin)}`;
 }
